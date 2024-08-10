2a1e4a2cc5d8a22b6b06ae34213f1f81bea0806d
RaulBirlem <raul.birlem@gmail.com>
2024-08-10 14:40:03 -0300

    Criando tabela link

--- src/main/java/com/rocketseat/planner/link/Link.java
+++ src/main/java/com/rocketseat/planner/link/Link.java
@@ -1,4 +1,4 @@
-package com.rocketseat.planner.activities;
+package com.rocketseat.planner.link;
 
 
 import com.rocketseat.planner.trip.Trip;
@@ -8,34 +8,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 import java.util.UUID;
 
 @Entity
-@Table(name = "activities")
+@Table(name = "links")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Activity {
+public class Link {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(name = "occurs_at", nullable = false)
-    private LocalDateTime occursAt;
+    @Column( nullable = false)
+    private String url;
+
     @Column(nullable = false)
     private String title;
 
     @ManyToOne
     @JoinColumn(name = "trip_id", nullable = false)
     private Trip trip;
-
-    public Activity(String title, String occursAt, Trip trip){
-        this.title = title;
-        this.occursAt = LocalDateTime.parse(occursAt, DateTimeFormatter.ISO_DATE_TIME);
-        this.trip = trip;
-    }
 }
